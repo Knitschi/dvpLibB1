ce198f400fd896c837ccf45ff05bfedc5ea0b03c
Knitschi <knitschi@freenet.de>
2020-11-21 13:37:34 +0100

    Adds first files.

---
+++ libB1.cpp
@@ -0,0 +1,11 @@
+#include <dvpLibB1/libB1.h>
+#include <iostream>
+
+namespace dvpLibB1
+{
+	void function()
+	{
+		std::cout << "BPackage function()"
+				  << "\n";
+	}
+}
---
+++ libB1.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <dvpLibB1/dvpLibB1_export.h>
+
+namespace dvpLibB1
+{
+	DVPLIBB1_EXPORT void function();
+}
