93de28be1bfc1620ce576e1b4d1e3a8164657dec
Knitschi <knitschi@freenet.de>
2020-11-22 15:55:08 +0100

    Adds a version postfix to the includes and the namespace.

--- libB1.h
+++ libB1.h
@@ -2,19 +2,19 @@
 
 #include <memory>
 
-#include <dvpLibC/ValueType.h>
-#include <dvpLibC/ComplexType.h>
+#include <dvpLibC_1_0_0/ValueType.h>
+#include <dvpLibC_1_0_0/ComplexType.h>
 
-#include <dvpLibB1/dvpLibB1_export.h>
+#include <dvpLibB1_2_0_0/dvpLibB1_export.h>
 
-namespace dvpLibB1
+namespace dvpLibB1_2_0_0
 {
-	DVPLIBB1_EXPORT void b1UseValueType(dvpLibC::ValueType value);
-	DVPLIBB1_EXPORT dvpLibC::ValueType b1ReturnValueType();
+	DVPLIBB1_EXPORT void b1UseValueType(dvpLibC_1_0_0::ValueType value);
+	DVPLIBB1_EXPORT dvpLibC_1_0_0::ValueType b1ReturnValueType();
 
-	DVPLIBB1_EXPORT void b1UseComplexType(dvpLibC::ComplexType* value);
-	DVPLIBB1_EXPORT dvpLibC::ComplexType* b1ReturnComplexType();
+	DVPLIBB1_EXPORT void b1UseComplexType(dvpLibC_1_0_0::ComplexType* value);
+	DVPLIBB1_EXPORT dvpLibC_1_0_0::ComplexType* b1ReturnComplexType();
 
-	DVPLIBB1_EXPORT void b1SetComplexType(std::unique_ptr<dvpLibC::ComplexType> value);
-	DVPLIBB1_EXPORT std::unique_ptr<dvpLibC::ComplexType> b1CreateComplexType();
+	DVPLIBB1_EXPORT void b1SetComplexType(std::unique_ptr<dvpLibC_1_0_0::ComplexType> value);
+	DVPLIBB1_EXPORT std::unique_ptr<dvpLibC_1_0_0::ComplexType> b1CreateComplexType();
 }
