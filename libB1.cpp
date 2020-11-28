d811b40dbcc2a26e5f526a61278ad111ef326c85
Knitschi <knitschi@freenet.de>
2020-11-28 17:36:20 +0100

    use macros for namespace

--- libB1.cpp
+++ libB1.cpp
@@ -8,40 +8,40 @@
 CPF_VERSIONED_NAMESPACE(dvpLibB1Ver)
 {
 
-	void b1UseValueType(dvpLibC_2_0_0::ValueType value)
+	void b1UseValueType(dvpLibCVer::ValueType value)
 	{
 		std::cout << "Called dpvLibB1::b1UseValueType() with argument " << value.foo << value.bar << "\n";
 	}
 
-	dvpLibC_2_0_0::ValueType b1ReturnValueType()
+	dvpLibCVer::ValueType b1ReturnValueType()
 	{
-		return dvpLibC_2_0_0::ValueType{ 3, 4 };
+		return dvpLibCVer::ValueType{ 3, 4 };
 	}
 
-	void b1UseComplexType(dvpLibC_2_0_0::ComplexType* value)
+	void b1UseComplexType(dvpLibCVer::ComplexType* value)
 	{
 		std::cout << "Called dpvLibB2::b2UseComplexType() with argument " << value->getFoo() << "\n";
 	}
 
-	std::unique_ptr<dvpLibC_2_0_0::ComplexType> globalComplexType = nullptr;
+	std::unique_ptr<dvpLibCVer::ComplexType> globalComplexType = nullptr;
 
-	dvpLibC_2_0_0::ComplexType* b1ReturnComplexType()
+	dvpLibCVer::ComplexType* b1ReturnComplexType()
 	{
 		if (!globalComplexType)
 		{
-			globalComplexType = std::make_unique<dvpLibC_2_0_0::ComplexType>();
+			globalComplexType = std::make_unique<dvpLibCVer::ComplexType>();
 		}
 		return globalComplexType.get();
 	}
 
-	void b1SetComplexType(std::unique_ptr<dvpLibC_2_0_0::ComplexType> value)
+	void b1SetComplexType(std::unique_ptr<dvpLibCVer::ComplexType> value)
 	{
 		globalComplexType = std::move(value);
 	}
 
-	std::unique_ptr<dvpLibC_2_0_0::ComplexType> b1CreateComplexType()
+	std::unique_ptr<dvpLibCVer::ComplexType> b1CreateComplexType()
 	{
-		return std::make_unique<dvpLibC_2_0_0::ComplexType>();
+		return std::make_unique<dvpLibCVer::ComplexType>();
 	}
 
 }
--- libB1.h
+++ libB1.h
@@ -11,12 +11,12 @@
 
 CPF_VERSIONED_NAMESPACE(dvpLibB1Ver)
 {
-	DVPLIBB1_2_0_0_EXPORT void b1UseValueType(dvpLibC_2_0_0::ValueType value);
-	DVPLIBB1_2_0_0_EXPORT dvpLibC_2_0_0::ValueType b1ReturnValueType();
+	DVPLIBB1_2_0_0_EXPORT void b1UseValueType(dvpLibCVer::ValueType value);
+	DVPLIBB1_2_0_0_EXPORT dvpLibCVer::ValueType b1ReturnValueType();
 
-	DVPLIBB1_2_0_0_EXPORT void b1UseComplexType(dvpLibC_2_0_0::ComplexType* value);
-	DVPLIBB1_2_0_0_EXPORT dvpLibC_2_0_0::ComplexType* b1ReturnComplexType();
+	DVPLIBB1_2_0_0_EXPORT void b1UseComplexType(dvpLibCVer::ComplexType* value);
+	DVPLIBB1_2_0_0_EXPORT dvpLibCVer::ComplexType* b1ReturnComplexType();
 
-	DVPLIBB1_2_0_0_EXPORT void b1SetComplexType(std::unique_ptr<dvpLibC_2_0_0::ComplexType> value);
-	DVPLIBB1_2_0_0_EXPORT std::unique_ptr<dvpLibC_2_0_0::ComplexType> b1CreateComplexType();
+	DVPLIBB1_2_0_0_EXPORT void b1SetComplexType(std::unique_ptr<dvpLibCVer::ComplexType> value);
+	DVPLIBB1_2_0_0_EXPORT std::unique_ptr<dvpLibCVer::ComplexType> b1CreateComplexType();
 }
