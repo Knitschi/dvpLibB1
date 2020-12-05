ddd9461437dd62bc9d682624de62daf8e59fc509
Knitschi <knitschi@freenet.de>
2020-12-05 16:00:27 +0100

    Switch to using an unversioned package namespace name to remove the version from the export macros and the export macro header filename.

--- libB1.h
+++ libB1.h
@@ -6,16 +6,16 @@
 #include CPF_VERSIONED_INCLUDE(dvpLibC, ValueType.h)
 #include CPF_VERSIONED_INCLUDE(dvpLibC, ComplexType.h)
 
-#include CPF_VERSIONED_INCLUDE(dvpLibB1, dvpLibB1_2_0_0_export.h)
+#include CPF_VERSIONED_INCLUDE(dvpLibB1, dvpLibB1_export.h)
 
 CPF_VERSIONED_NAMESPACE(dvpLibB1)
 {
-	DVPLIBB1_2_0_0_EXPORT void b1UseValueType(dvpLibC::ValueType value);
-	DVPLIBB1_2_0_0_EXPORT dvpLibC::ValueType b1ReturnValueType();
+	DVPLIBB1_EXPORT void b1UseValueType(dvpLibC::ValueType value);
+	DVPLIBB1_EXPORT dvpLibC::ValueType b1ReturnValueType();
 
-	DVPLIBB1_2_0_0_EXPORT void b1UseComplexType(dvpLibC::ComplexType* value);
-	DVPLIBB1_2_0_0_EXPORT dvpLibC::ComplexType* b1ReturnComplexType();
+	DVPLIBB1_EXPORT void b1UseComplexType(dvpLibC::ComplexType * value);
+	DVPLIBB1_EXPORT dvpLibC::ComplexType* b1ReturnComplexType();
 
-	DVPLIBB1_2_0_0_EXPORT void b1SetComplexType(std::unique_ptr<dvpLibC::ComplexType> value);
-	DVPLIBB1_2_0_0_EXPORT std::unique_ptr<dvpLibC::ComplexType> b1CreateComplexType();
+	DVPLIBB1_EXPORT void b1SetComplexType(std::unique_ptr<dvpLibC::ComplexType> value);
+	DVPLIBB1_EXPORT std::unique_ptr<dvpLibC::ComplexType> b1CreateComplexType();
 }
